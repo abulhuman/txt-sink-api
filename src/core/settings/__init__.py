f881a3bdb3d399949297ed10e2cde2a7a4f45626
Adem Mohammed <abulhuman.dev@gmail.com>
2024-12-19 17:09:24 -0800

    Setup logging with custom format

--- src/core/settings/__init__.py
+++ src/core/settings/__init__.py
@@ -9,28 +9,28 @@ from split_settings.tools import include, optional
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 # Namespacing our env vars to avoid conflicts
-ENV_VAR_SETTINGS_PREFIX = 'TXT_SINK_SETTINGS_'
+ENV_VAR_SETTINGS_PREFIX = "TXT_SINK_SETTINGS_"
 # The path to the local settings file
-LOCAL_SETTINGS_PATH = os.getenv(f'{ENV_VAR_SETTINGS_PREFIX}LOCAL_SETTINGS_PATH')
+LOCAL_SETTINGS_PATH = os.getenv(f"{ENV_VAR_SETTINGS_PREFIX}LOCAL_SETTINGS_PATH")
 
 if not LOCAL_SETTINGS_PATH:
     # We dedicate local/settings.unittests.py to have reproducible unittest runs
-    LOCAL_SETTINGS_PATH = 'local/settings.dev.py'
+    LOCAL_SETTINGS_PATH = "local/settings.dev.py"
     # LOCAL_SETTINGS_PATH = f'local/settings{".unittests" if is_pytest_running() else ".dev"}.py'
 
 if not os.path.isabs(LOCAL_SETTINGS_PATH):
     LOCAL_SETTINGS_PATH = str(BASE_DIR / LOCAL_SETTINGS_PATH)
 
 include(
-    'base.py',
-    # 'logging.py',
+    "base.py",
+    "setting_logging.py",
     # 'rest_framework.py',
     # 'channels.py',
     # 'aws.py',
-    'custom.py',
+    "custom.py",
     optional(LOCAL_SETTINGS_PATH),
-    'envvars.py',
-    'docker.py',
+    "envvars.py",
+    "docker.py",
 )
 
 # if not is_pytest_running():
--- src/core/settings/templates/settings.dev.py
+++ src/core/settings/templates/settings.dev.py
@@ -6,3 +6,11 @@ SECRET_KEY = "django-insecure-4#g%fqt_h=uu9bs81iy#pt0jz1rc50f*n_!x4*tl0ol!%#8lv9
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+LOGGING['formatters']['colored'] = {  # type: ignore # noqa: F821
+    '()': 'colorlog.ColoredFormatter',
+    'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
+}
+LOGGING['loggers']['src']['level'] = 'DEBUG'  # type: ignore # noqa: F821
+LOGGING['handlers']['console']['level'] = 'DEBUG'  # type: ignore # noqa: F821
+LOGGING['handlers']['console']['formatter'] = 'colored'  # type: ignore # noqa: F821
