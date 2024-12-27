3aeebf4f97e21ff30f46162f32eca1aee2301472
Adem Mohammed <abulhuman.dev@gmail.com>
2024-12-27 12:06:22 -0800

    feat(settings): Update production settings to use environment variables with fallback options

--- src/core/settings/prod.py
+++ src/core/settings/prod.py
@@ -1,4 +1,4 @@
-""" Production settings """
+"""Production settings"""
 
 import os
 
@@ -6,16 +6,16 @@ from dotenv import load_dotenv
 
 from . import BASE_DIR
 
-load_dotenv(dotenv_path=str(BASE_DIR / '.env.production'))  # type: ignore # noqa: F821 # pylint: disable=E0602
+load_dotenv(dotenv_path=str(BASE_DIR / ".production.env"), override=True)  # type: ignore # noqa: F821 # pylint: disable=E0602
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
-ADMIN = os.getenv('ADMIN')
+SECRET_KEY = os.getenv("SECRET_KEY", os.getenv("TXT_SINK_SETTINGS_SECRET_KEY"))
+DEBUG = os.getenv("DEBUG", os.getenv("TXT_SINK_SETTINGS_DEBUG")) == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", os.getenv("TXT_SINK_SETTINGS_ALLOWED_HOSTS", "")).split(",")
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", os.getenv("TXT_SINK_SETTINGS_CORS_ALLOWED_ORIGINS",
+                                                                   "")).split(",")
+ADMIN = os.getenv("ADMIN", os.getenv("TXT_SINK_SETTINGS_ADMIN"))
 
-AWS_SM_RDS_SECRET_ID = os.getenv('AWS_SM_RDS_SECRET_ID')
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
+AWS_SM_RDS_SECRET_ID = os.getenv("AWS_SM_RDS_SECRET_ID", os.getenv("TXT_SINK_SETTINGS_AWS_SM_RDS_SECRET_ID"))
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", os.getenv("TXT_SINK_SETTINGS_AWS_ACCESS_KEY_ID"))
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", os.getenv("TXT_SINK_SETTINGS_AWS_SECRET_ACCESS_KEY"))
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME", os.getenv("TXT_SINK_SETTINGS_AWS_STORAGE_BUCKET_NAME"))
