a586ecd206dd26973d06d65b2455a7ecac477207
Adem Mohammed <abulhuman.dev@gmail.com>
2024-12-28 00:36:23 -0800

    feat(middlewares): Enhance ALLOWED_HOSTS handling with dynamic IP fetching and CIDR ranges

--- scripts/docker.entrypoint.sh
+++ scripts/docker.entrypoint.sh
@@ -13,5 +13,29 @@ echo 'Creating superuser...'
 DJANGO_SUPERUSER_PASSWORD=$TXT_SINK_SETTINGS_DJANGO_SUPERUSER_PASSWORD $RUN_MANAGE_PY createsuperuser --no-input \
 --username $TXT_SINK_SETTINGS_DJANGO_SUPERUSER_USERNAME --email $TXT_SINK_SETTINGS_DJANGO_SUPERUSER_EMAIL || echo 'Superuser already exists.'
 
-exec /root/.local/bin/poetry run gunicorn src.core.asgi:application -k uvicorn_worker.UvicornWorker -b :$TXT_SINK_SETTINGS_GUNICORN_PORT
+echo "Fetching IP addresses..."
+HOST_IPS=$(hostname -I)
+IFS=' ' read -r -a HOST_IP_ARRAY <<< "$HOST_IPS"
+
+ALLOWED_HOSTS="[$TXT_SINK_SETTINGS_ALLOWED_HOSTS"
+for HOST_IP in "${HOST_IP_ARRAY[@]}"; do
+  ALLOWED_HOSTS="$ALLOWED_HOSTS, '$HOST_IP'"
+done
+
+# Append CIDR range 10.0.10.0/24 and range 10.0.11.0/24
+for i in {0..255}; do
+  ALLOWED_HOSTS="$ALLOWED_HOSTS, '10.0.10.$i'"
+  ALLOWED_HOSTS="$ALLOWED_HOSTS, '10.0.11.$i'"
+done
+
+ALLOWED_HOSTS="$ALLOWED_HOSTS]"
+
+# Export ALLOWED_HOSTS
+export ALLOWED_HOSTS
+
+echo "Updated ALLOWED_HOSTS#Count=${#ALLOWED_HOSTS}"
+
+# Pass environment variable to Gunicorn
+TXT_SINK_SETTINGS_ALLOWED_HOSTS="$ALLOWED_HOSTS" exec  /root/.local/bin/poetry run gunicorn src.core.asgi:application -k uvicorn_worker.UvicornWorker -b :$TXT_SINK_SETTINGS_GUNICORN_PORT
+
 
--- src/core/middlewares.py
+++ src/core/middlewares.py
@@ -1,7 +1,6 @@
 """Middleware to add dynamic hosts to ALLOWED_HOSTS from our alb subnet"""
 
 from django.conf import settings
-from django.core.exceptions import DisallowedHost
 
 TXT_SINK_APP_SUBNET_A_CIDR_PREFIX = "10.0.10."
 TXT_SINK_APP_SUBNET_B_CIDR_PREFIX = "10.0.11."
@@ -14,10 +13,10 @@ class ALBSubnetDynamicAllowedHostsMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        print("[__call__] ALLOWED: ", settings.ALLOWED_HOSTS)
         host = request.get_host().split(":")[0]
-        if not host.startswith(TXT_SINK_APP_SUBNET_A_CIDR_PREFIX
-                               ) or not host.startswith(TXT_SINK_APP_SUBNET_B_CIDR_PREFIX):
-            raise DisallowedHost("Host not allowed")
-        if host and host not in settings.ALLOWED_HOSTS:
+        if (
+            host.startswith(TXT_SINK_APP_SUBNET_A_CIDR_PREFIX) or host.startswith(TXT_SINK_APP_SUBNET_B_CIDR_PREFIX)
+        ) and host not in settings.ALLOWED_HOSTS:
             settings.ALLOWED_HOSTS.append(host)
         return self.get_response(request)
